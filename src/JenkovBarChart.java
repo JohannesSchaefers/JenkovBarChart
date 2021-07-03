ca406edd391412e0d56bd4b48b29fc24ae7a064c
johannesschaefers <info@iconicity.de>
2021-07-03 11:47:38 +0200

    nach reverts 3

--- src/JenkovBarChart.java
+++ src/JenkovBarChart.java
@@ -92,6 +92,7 @@ public class JenkovBarChart extends Application {
                                     System.out.println(aString[0] + " Volt");
                                     System.out.println("nach reverts");
                                     System.out.println("nach reverts2");
+                                    System.out.printf("nach reverts3");
                                 }
                             }
                         }
